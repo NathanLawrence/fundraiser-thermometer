365fe02296b3c211ddf48e58ee559aeecb36c8cb
Nathan Lawrence <nathan@nathanlawrence.org>
2015-10-24 16:13:40 -0500

    Created Admin interface for Therm object

--- admin.py
+++ admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Therm
+
+class ThermAdmin(admin.ModelAdmin):
+	list_display =  ['name','heading','fundGoal','fundCurrent']
+
+admin.site.register(Therm,ThermAdmin)
